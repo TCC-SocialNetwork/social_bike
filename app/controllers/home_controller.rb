f43ab88b8209b7d6351cce7249bab38bbd6151c6
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2016-06-04 16:40:20 -0300

    Searchs and relationships #2 #5
    Signed-off-by: Álex Mesquita <alex.mesquita06@hotmail.com>
    Signed-off-by: Jefferson Xavier <jeffersonx.xavier@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,61 @@
 class HomeController < ApplicationController
+  before_action :set_user, only: [:add_friend, :accept_friend, :remove_friend]
   before_action :authenticate_user!
 
   def index
+    @users ||= Array.new
+    @events ||= Array.new
+    @friends = current_user.relationships("friend")
+    @requests_friendship = current_user.relationships("friend", false, "other")
+    @requests_open = current_user.relationships("friend", false, "self")
   end
+
+  def search
+    @search = params[:search]
+    map = { username: @search, email: @search }
+
+    if params[:commit] == 'Continuar'
+      hash = current_user.graph.search(map, true)
+    else
+      hash = current_user.graph.search(map)
+    end
+
+    @users = hash[:users]
+    @events = hash[:events]
+
+    render :index
+  end
+
+  def add_friend
+    if current_user.create_relationship(@user, "friend")
+      message = "adicionado"
+    else
+      message = "não adicionado"
+    end
+
+    redirect_to root_url, notice: message
+  end
+
+  def accept_friend
+    if current_user.confirm_relationship(@user, "friend")
+      mensagem = "confirmado"
+    else
+      mensagem = "não confirmado"
+    end
+    redirect_to root_url, notice: mensagem
+  end
+
+  def remove_friend
+    if current_user.remove_relationship(@user, "friend")
+      mensagem = "removido"
+    else
+      mensagem = "não removido"
+    end
+    redirect_to root_url, notice: mensagem
+  end
+
+  private
+    def set_user
+      @user = SocialFramework::User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
     controllers: {sessions: 'users/sessions',
                   registrations: 'users/registrations',
                   passwords: 'users/passwords'}
+
+  post :search, to: 'home#search'
+  post 'add_friend/:id', to: 'home#add_friend', as: :add_friend
+  post 'accept_friend/:id', to: 'home#accept_friend', as: :accept_friend
+  post 'remove_friend/:id', to: 'home#remove_friend', as: :remove_friend
 end
